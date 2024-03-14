15d44cc1775e85ba949ec0df3bf03adf0c8a59d7
KKJ1000 <kwangjoon1733@naver.com>
2024-03-14 11:12:36 +0900

    [Feat] 스크립터블 Event 테스트까지

---
+++ UnityProject_2024_2_B_K/Assets/Scripts/EventChannel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; //이벤트 사용
+[CreateAssetMenu(fileName = "NewEventChannel", menuName = "Events/Event Chaanel")]
+public class EventChannel : ScriptableObject
+{
+    public UnityEvent OnEventRaised;
+    public void RaiseEvent()
+    {
+        if(OnEventRaised != null)
+        {
+            OnEventRaised.Invoke();
+        }
+    }
+}
